3ffdef945ba4d76d855f63090ee5b9200a06d9e6
Hyunsung <jodog0412@gmail.com>
2024-07-26 22:50:56 +0900

    feat: deploy fastapi app with openai api

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from openai import OpenAI
-import uvicorn
+import os
 
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-from pydantic_settings import BaseSettings
+from pydantic import BaseModel
+from dotenv import load_dotenv
 
-class Settings(BaseSettings):
-    OPENAI_API_KEY: str = 'OPENAI_API_KEY'
+load_dotenv()
+class Settings(BaseModel):
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
 
-    class Config:
-        env_file = '.env'
+class Config:
+    env_file = '.env'
 
 settings = Settings()
 client = OpenAI(
@@ -25,9 +27,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
-def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
+def return_index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @app.post("/", response_class=HTMLResponse)
 def index(request: Request, animal: str= Form(...)):
@@ -40,20 +41,16 @@ def index(request: Request, animal: str= Form(...)):
         temperature = 0.6,
     )
     result = response.choices[0].message.content
-    return templates.TemplateResponse("index.html", {"request": request, "result": result})
+    return templates.TemplateResponse(request=request, name="index.html", context={"result": result})
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    answer = f"""Suggest three names for an animal that is a superhero.
+    Animal: Cat
+    Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+    Animal: Dog
+    Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+    Animal: {animal.capitalize()}
+    Names:"""
+    return answer
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
-
-if __name__ == "__main__":
-    uvicorn.run('app:app', host="localhost", port=5001, reload=True)
